852b3301b74ad53d927ad053c3d499f6d9ced0e2
fgs1991 <652810758@qq.com>
2018-04-23 20:43:56 +0800

    版本改动

---
+++ springboot-chapter-b/src/main/java/com/forezp/ChapterBApplication.java
@@ -0,0 +1,13 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapterBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapterBApplication.class, args);
+	}
+
+}
